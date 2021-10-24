230e9950794411e892349d76984d8e2c9a039b6c
roli72 <dohi_roland@yahoo.com>
2021-10-24 22:03:58 +0300

    modificare

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!");
 
+        System.out.println("Alexandru Roland Dohi");
+
+        int myint = 10;
+
+        int myint2 = 3;
+
+        int result = myint + myint2;
+        System.out.println(result);
+
+        double result1 = (double) myint / myint2;
+        System.out.println(result1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
