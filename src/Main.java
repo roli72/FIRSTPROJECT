63f22e35ea7dd4e18ea3833144ee639fb00b654f
roli72 <dohi_roland@yahoo.com>
2021-10-24 22:37:01 +0300

    modificare

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,26 @@ public class Main {
         double result1 = (double) myint / myint2;
         System.out.println(result1);
 
+        int n1 = -5;
+        int n2 = 8;
+        int n3 = 6;
+
+        int result2 = n1 + (n2 * n3);
+        System.out.println(result2);
+
+        int n4 = 55;
+        int n5 = 9;
+
+        int result3 = (n4 + n5) % n5;
+        System.out.println(result3);
+
+
+
+
+
+
+
+
 
 
 
